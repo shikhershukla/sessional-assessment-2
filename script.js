aa6eb860bacbe83a563fe284e030d3273417161f
Shikher Shukla <shikher.shukla02@gmail.com>
2022-08-11 14:07:31 +0530

    add pagination

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+let data,
+page = 1;
+
+// fetching data
 const xhr = new XMLHttpRequest();
 const source =
   "https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09";
 xhr.open("GET", source, true);
 xhr.onreadystatechange = () => {
   if (xhr.readyState === 4 && xhr.status === 200) {
-    const response = JSON.parse(xhr.responseText);
-    console.log(response);
+    data = JSON.parse(xhr.responseText);
+    // console.log(response);
     let output = "";
-    for (let i = 0; i < response.length; i++) {
+    for (let i = 0; i < Math.min(data.length, 10); i++) {
       output += `
         <div class="box">
-            <img src="https://picsum.photos/250/?random=${i}"> 
+            <img src="https://picsum.photos/250/?random=${i}">
             <div class="content">
-                <p class="user">User - ${response[i].userId}</p>
-                <p class="title">${response[i].title}</p>
-                <p class="body">${response[i].body}</p>
+                <p class="user">User - ${data[i].userId}</p>
+                <p class="title">${data[i].title}</p>
+                <p class="body">${data[i].body}</p>
             </div>
         </div>`;
     }
@@ -22,3 +26,29 @@ xhr.onreadystatechange = () => {
   }
 };
 xhr.send();
+
+// implementing pagination
+let pagination = new tui.Pagination("pagination", {
+  totalItems: 100,
+});
+
+pagination.on("beforeMove", function (eventData) {
+  page = eventData.page;
+  updateContent();
+});
+
+function updateContent() {
+  let output = "";
+  for (let i = (page - 1) * 10; i < page * 10; i++) {
+    output += `
+        <div class="box">
+            <img src="https://picsum.photos/250/?random=${i}">
+            <div class="content">
+                <p class="user">User - ${data[i].userId}</p>
+                <p class="title">${data[i].title}</p>
+                <p class="body">${data[i].body}</p>
+            </div>
+        </div>`;
+  }
+  document.querySelector(".container").innerHTML = output;
+}

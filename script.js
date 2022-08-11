e4e7a9bb933d8b4a3037f395913f18eca2e03326
Shikher Shukla <shikher.shukla02@gmail.com>
2022-08-11 16:15:05 +0530

    add logo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let data,
-page = 1;
+  page = 1;
 
 // fetching data
 const xhr = new XMLHttpRequest();
@@ -9,20 +9,7 @@ xhr.open("GET", source, true);
 xhr.onreadystatechange = () => {
   if (xhr.readyState === 4 && xhr.status === 200) {
     data = JSON.parse(xhr.responseText);
-    // console.log(response);
-    let output = "";
-    for (let i = 0; i < Math.min(data.length, 10); i++) {
-      output += `
-        <div class="box">
-            <img src="https://picsum.photos/250/?random=${i}">
-            <div class="content">
-                <p class="user">User - ${data[i].userId}</p>
-                <p class="title">${data[i].title}</p>
-                <p class="body">${data[i].body}</p>
-            </div>
-        </div>`;
-    }
-    document.querySelector(".container").innerHTML = output;
+    updateContent();
   }
 };
 xhr.send();
